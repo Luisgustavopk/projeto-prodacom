02eef1499461b15a2124cae944c8fdec5f3ae44d
Luisgustavopk <luisgustavoxavier1234@gmail.com>
2026-07-10 17:11:11 -0300

    fix: pequenos ajuste no frontend

--- prodacom-web/src/App.jsx
+++ prodacom-web/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import AdminChat from './pages/AdminChat';
 
 export default function App() {
   return (
