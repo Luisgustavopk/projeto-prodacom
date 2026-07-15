a3e885fcf4787e2b5badc3e3a5eb18741e71c993
Luisgustavopk <luisgustavoxavier1234@gmail.com>
2026-07-15 15:40:55 -0300

    fix: ajuste temporario na url do api.js do admin-chat

--- prodacom-admin-chat/src/config/api.js
+++ prodacom-admin-chat/src/config/api.js
@@ -1,5 +1,5 @@
 const API_URL = import.meta.env.MODE === 'production' 
-  ? 'https://api.prodacom.com.br' // Sua futura URL de produção
+  ? 'http://localhost:3001' // Sua futura URL de produção
   : 'http://localhost:3001';
 
 export default API_URL;
